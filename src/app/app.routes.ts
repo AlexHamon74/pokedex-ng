0346dc45f72d5c9c2be74eca4c0400c93189ccd2
alex <alex.hmn18@gmail.com>
2025-04-01 09:31:36 +0200

    Création composant PokemonList

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'pokemons', component:PokemonListComponent},
+    {
+        path: '',
+        redirectTo: '/pokemons',
+        pathMatch: 'full',
+    },
+
+];
